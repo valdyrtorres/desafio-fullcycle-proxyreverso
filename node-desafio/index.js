4f6511712fc6ad54be5cc0537bb0cc7543534957
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-06-21 22:33:40 -0300

    Sem o dockerize funciona

--- node-desafio/index.js
+++ node-desafio/index.js
@@ -7,15 +7,19 @@ const config = {
     password: 'root',
     database: 'nodedb'
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Valdir')`
-connection.query(sql)
-connection.end()
+let usuarios = ["Valdir", "Torres", "Borges", "Rildav", "Gesbor"];
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+    res.send('<h1>Full Cycle Rocks!</h1>')
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+
+    let usuario = usuarios[(Math.floor(Math.random() * usuarios.length))];
+    let sql = `INSERT INTO people(name) values('${usuario}')`
+    
+    connection.query(sql)
+    connection.end()
 })
 
 app.listen(port, ()=> {
