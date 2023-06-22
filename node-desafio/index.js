17a4eed4439c08ad0e790f09b16b0aee5116e3a6
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-06-22 00:24:10 -0300

    listando quem acessa o node

--- node-desafio/index.js
+++ node-desafio/index.js
@@ -11,14 +11,38 @@ const config = {
 let usuarios = ["Valdir", "Torres", "Borges", "Rildav", "Gesbor"];
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
-    const mysql = require('mysql')
-    const connection = mysql.createConnection(config)
+    
+    const mysql = require('mysql');
+    const connection = mysql.createConnection(config);
 
     let usuario = usuarios[(Math.floor(Math.random() * usuarios.length))];
-    let sql = `INSERT INTO people(name) values('${usuario}')`
+    var sql = `INSERT INTO people(name) values('${usuario}')`;
     
-    connection.query(sql)
+    connection.query(sql);
+
+    var conteudo = '<h1>Full Cycle Rocks!</h1>' + '<h3>Lista de Usuários<h3>';
+
+    sql = `SELECT name FROM people`;
+    //console.log(sql);
+    //var [rows] = connection.query(sql);
+
+    //console.log(rows)
+
+    var linhas;
+
+    connection.query(sql, function(err, linhas){ 
+        //const pares = JSON.parse(JSON.stringify(linhas));
+
+        for (var i = 0; i < linhas.length; i++){
+            conteudo = conteudo + linhas[i].name + '<br>';
+        }
+
+        console.log(conteudo);
+        res.send(conteudo);
+    });
+
+    
+
     connection.end()
 })
 
