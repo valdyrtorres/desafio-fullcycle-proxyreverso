07432a28f819c786d61c4d6bec869785502f4920
Valdir Torres <valdyrtorres@yahoo.com.br>
2023-06-24 18:27:44 -0300

    troubleshooting: rodando somente o node - OK

--- node-desafio/index.js
+++ node-desafio/index.js
@@ -9,78 +9,75 @@ const config = {
 };
 
 const mysql = require('mysql');
-const conn = mysql.createConnection(config);
+// const conn = mysql.createConnection(config);
 
-const table_exists_query = `
-  SELECT COUNT(*) table_exists 
-    FROM information_schema.TABLES 
-   WHERE TABLE_SCHEMA = DATABASE()
-     AND TABLE_TYPE = 'BASE TABLE'
-     AND TABLE_NAME = 'people';`;
+// const table_exists_query = `
+//   SELECT COUNT(*) table_exists 
+//     FROM information_schema.TABLES 
+//    WHERE TABLE_SCHEMA = DATABASE()
+//      AND TABLE_TYPE = 'BASE TABLE'
+//      AND TABLE_NAME = 'people';`;
 
-var tabela_people_existe = false;
+// var tabela_people_existe = false;
 
-conn.query(table_exists_query, function(err, linhas){ 
+// conn.query(table_exists_query, function(err, linhas){ 
 
-    console.log(linhas);
+//     console.log(linhas);
 
-    tabela_people_existe = linhas[0].table_exists > 0 ? true : false
+//     tabela_people_existe = linhas[0].table_exists > 0 ? true : false
 
-    console.log(linhas[0].table_exists);
+//     console.log(linhas[0].table_exists);
 
-    console.log(tabela_people_existe);
+//     console.log(tabela_people_existe);
 
-    if(!tabela_people_existe) {
-        console.log('ENTREI');
-        var sqlCriar = `CREATE TABLE people(id int not null auto_increment, name varchar(255), primary key(id))`;
-        conn.query(sqlCriar, function(err, rows) {
-            if(err) {
-                console.log('Erro na criação da tabela people:' + err.message);
-                conn.end();
-            }
-            else
-                console.log('Tabela people criada');
-        });
-    }
+//     if(!tabela_people_existe) {
+//         console.log('ENTREI');
+//         var sqlCriar = `CREATE TABLE people(id int not null auto_increment, name varchar(255), primary key(id))`;
+//         conn.query(sqlCriar, function(err, rows) {
+//             if(err) {
+//                 console.log('Erro na criação da tabela people:' + err.message);
+//                 conn.end();
+//             }
+//             else
+//                 console.log('Tabela people criada');
+//         });
+//     }
 
-    conn.end();
+//     conn.end();
     
-});
+// });
 
 let usuarios = ["Valdir", "Torres", "Borges", "Rildav", "Gesbor"];
 
 app.get('/', (req,res) => {
 
-    const connection = mysql.createConnection(config);
+    // const connection = mysql.createConnection(config);
 
 
-    let usuario = usuarios[(Math.floor(Math.random() * usuarios.length))];
-    var sql = `INSERT INTO people(name) values('${usuario}')`;
+    // let usuario = usuarios[(Math.floor(Math.random() * usuarios.length))];
+    // var sql = `INSERT INTO people(name) values('${usuario}')`;
     
-    connection.query(sql);
+    // connection.query(sql);
 
     var conteudo = '<h1>Full Cycle Rocks!</h1>' + '<h3>Lista de Usuários<h3>';
+    res.send(conteudo);
 
-    sql = `SELECT name FROM people`;
-    //console.log(sql);
-    //var [rows] = connection.query(sql);
+    // sql = `SELECT name FROM people`;
 
-    //console.log(rows)
+    // var linhas;
 
-    var linhas;
+    // connection.query(sql, function(err, linhas){ 
+    //     //const pares = JSON.parse(JSON.stringify(linhas));
 
-    connection.query(sql, function(err, linhas){ 
-        //const pares = JSON.parse(JSON.stringify(linhas));
+    //     for (var i = 0; i < linhas.length; i++){
+    //         conteudo = conteudo + linhas[i].name + '<br>';
+    //     }
 
-        for (var i = 0; i < linhas.length; i++){
-            conteudo = conteudo + linhas[i].name + '<br>';
-        }
+    //     console.log(conteudo);
+    //     res.send(conteudo);
+    // });
 
-        console.log(conteudo);
-        res.send(conteudo);
-    });
-
-    connection.end()
+    // connection.end()
 })
 
 app.listen(port, ()=> {
